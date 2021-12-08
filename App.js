8a8ba8e3930e975aefd7cd2cd45583bb05ccfc58
piyumaldissanayake <piyumaldissanayake@outlook.com>
2021-12-08 12:20:29 +1100

    app layout setup

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container} >
-      <Text>Our React Native Blank Canvas</Text>
-    </View>
+    <>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search} >
+        <Text>Search</Text>
+      </View>
+      <View style={styles.list} >
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
+    <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight // only for andriod. Does not work in ios
+  },
+  search: {
+    padding: '5%',
+    backgroundColor: 'green',
+  },
+  list: {
+    flex: 1,
+    padding: '5%',
+    backgroundColor: 'blue',
+  },
 });
