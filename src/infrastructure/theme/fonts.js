018bb7cfabbc1e4fc9c93a57a2bb6f35c5bedfa0
piyumaldissanayake <piyumaldissanayake@outlook.com>
2022-01-19 15:02:18 +1100

    finishing up retaurant card and adding rating and open and closed labels

--- src/infrastructure/theme/fonts.js
+++ src/infrastructure/theme/fonts.js
@@ -2,15 +2,13 @@ export const fonts = {
     body: "Oswald_400Regular",
     heading: "Lato_400Regular",
     monospace: "Oswald_400Regular",
-  };
- 
-  export const fontWeights = {
+  }; 
+export const fontWeights = {
     regular: 400,
     medium: 500,
     bold: 700,
   };
- 
-  export const fontSizes = {
+export const fontSizes = {
     caption: "12px",
     button: "14px",
     body: "16px",
